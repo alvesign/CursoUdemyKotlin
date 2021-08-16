bffeacf21f5be323cce2ee2e3ea27b0c569fc114
Márcio Alves <marcio.webdesigner@gmail.com>
2021-08-16 00:00:20 -0300

    Exercicios de Kotlin  - Operadores Relacionais

---
+++ src/main/kotlin/exercicios/programacao/kotlin/OperadoresRelacionaisLogicos.kt
@@ -0,0 +1,18 @@
+package exercicios.programacao.kotlin
+
+fun main(args: Array<String>){
+    println(5 == 5)
+    println(5 != 6)
+    println(5 > 5)
+    println(5 < 5)
+    println(5 <= 5)
+    println(5 >= 5)
+    println(false && false)
+    println(true && true)
+    println(true && true)
+    println(true || false)
+    println(false || false)
+    var notaProfessor = 5 //0-10
+    var notaProvaGeral = 5 //0-10
+    println( notaProfessor > 6 || notaProvaGeral >= 6)
+}
