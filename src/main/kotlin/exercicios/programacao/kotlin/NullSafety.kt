f011d72404de8433e0a2937f2f6a64a4db08e973
Márcio Alves <marcio.webdesigner@gmail.com>
2021-08-16 23:00:11 -0300

    Exercicios de Kotlin  - Null Safety

---
+++ src/main/kotlin/exercicios/programacao/kotlin/NullSafety.kt
@@ -0,0 +1,9 @@
+package exercicios.programacao.kotlin
+
+fun main(args: Array<String>) {
+    var texto: String? = null
+    texto = "Márcio"
+
+    //if (texto != null){
+        println(texto?.length)
+    }
